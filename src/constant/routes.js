d2c98d730fe589dd857ae45cf61176706498ed3a
Alexandre Disdier <28678511+alexdisdier@users.noreply.github.com>
2019-08-24 00:49:37 +0200

    test and server up and running

--- src/constant/routes.js
+++ src/constant/routes.js
@@ -1,4 +1,4 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 exports.ROUTE_URL = "/url";
 exports.ROUTE_UPDATE = "/update";
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+const app = require("./app");
+
+import { Request, Response } from "express";
+
+/*
+  SERVER
+*/
+
+app.all("*", (req: Request, res: Response) => {
+  res.status(404).send("Page not found");
+});
+
+app.use((err: any, req: Request, res: Response, next: any) => {
+  if (res.statusCode === 200) res.status(400);
+  console.log(err);
+  res.json({ error: err });
+});
+
+app.listen(process.env.PORT || 3001, () => {
+  console.log("server started");
+});
