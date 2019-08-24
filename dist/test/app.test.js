adf01f6f47956af14e81d3a0676dac38c337fd7a
Alexandre Disdier <28678511+alexdisdier@users.noreply.github.com>
2019-08-24 22:46:44 +0200

    tidying up app test file

--- dist/test/app.test.js
+++ dist/test/app.test.js
@@ -12,70 +12,71 @@ const app = require("../src/app");
 const request = require("supertest");
 const http = require("http");
 const routes_1 = require("../src/constant/routes");
-const server = http.createServer(app);
-describe("starting and stopping server", () => {
+describe("App.ts testing", () => {
     let server;
-    beforeAll((done) => __awaiter(this, void 0, void 0, function* () {
+    beforeAll(done => {
         // do something before anything else runs
-        server = http.createServer(app);
+        server = http.createServer((req, res) => {
+            res.write("ok");
+            res.end();
+        });
         server.listen(done);
         console.log("Jest starting!");
-    }));
+    });
     // close the server after each test
     afterAll(done => {
         server.close(done);
         console.log("server closed!");
     });
-    test("my test", () => __awaiter(this, void 0, void 0, function* () { }));
-});
-describe("basic route tests", () => {
-    test("get home route GET /", () => __awaiter(this, void 0, void 0, function* () {
-        const response = yield request(app).get("/");
-        const home = '{"home":{"message":"Welcome to AD SHORT-URL API","urlList":"/url"}}';
-        expect(response.status).toEqual(200);
-        expect(response.text).toContain(home);
-    }));
-});
-/**
- * Testing root endpoint
- */
-describe("GET route /", () => {
-    it("responds with a welcoming message", done => {
-        request(app)
-            .get("/")
-            .set("Accept", "application/json")
-            .expect("Content-Type", /json/)
-            .expect(200, done);
+    describe("basic route tests", () => {
+        test("get home route GET /", () => __awaiter(this, void 0, void 0, function* () {
+            const response = yield request(app).get("/");
+            const home = '{"home":{"message":"Welcome to AD SHORT-URL API","urlList":"/url"}}';
+            expect(response.status).toEqual(200);
+            expect(response.text).toContain(home);
+        }));
     });
-});
-/**
- * Testing get all url
- */
-describe(`GET route ${routes_1.ROUTE_URL}`, () => {
-    it("respond with json containing a list of all urls", done => {
-        request(app)
-            .get(routes_1.ROUTE_URL)
-            .set("Accept", "application/json")
-            .expect("Content-Type", /json/)
-            .expect(200, done);
+    /**
+     * Testing root endpoint
+     */
+    describe("GET route /", () => {
+        it("responds with a welcoming message", done => {
+            request(app)
+                .get("/")
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(200, done);
+        });
     });
-    // it("throws if network is down", done => {
-    //   request(app)
-    //     .get(ROUTE_URL)
-    //     .set("DENY", "application/json")
-    //     .expect("Content-Type", /json/)
-    //     .expect(500, done);
-    // });
-});
-/**
- * Testing get url redirection
- */
-describe("GET /:id", function () {
-    it("respond with json containing a single url", function (done) {
-        request(app)
-            .get("/url")
-            .set("Accept", "application/json")
-            .expect("Content-Type", /json/)
-            .expect(200, done);
+    /**
+     * Testing get all url
+     */
+    describe(`GET route ${routes_1.ROUTE_URL}`, () => {
+        it("respond with json containing a list of all urls", done => {
+            request(app)
+                .get(routes_1.ROUTE_URL)
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(200, done);
+        });
+        // it("throws if network is down", done => {
+        //   request(app)
+        //     .get(ROUTE_URL)
+        //     .set("DENY", "application/json")
+        //     .expect("Content-Type", /json/)
+        //     .expect(500, done);
+        // });
+    });
+    /**
+     * Testing get url redirection
+     */
+    describe("GET /:id", function () {
+        it("respond with json containing a single url", function (done) {
+            request(app)
+                .get("/url")
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(200, done);
+        });
     });
 });
--- test/app.test.ts
+++ test/app.test.ts
@@ -3,14 +3,15 @@ import * as request from "supertest";
 import * as http from "http";
 import { ROUTE_URL, ROUTE_UPDATE } from "../src/constant/routes";
 
-const server = http.createServer(app);
-
-describe("starting and stopping server", () => {
+describe("App.ts testing", () => {
   let server;
 
-  beforeAll(async done => {
+  beforeAll(done => {
     // do something before anything else runs
-    server = http.createServer(app);
+    server = http.createServer((req, res) => {
+      res.write("ok");
+      res.end();
+    });
     server.listen(done);
     console.log("Jest starting!");
   });
@@ -21,62 +22,60 @@ describe("starting and stopping server", () => {
     console.log("server closed!");
   });
 
-  test("my test", async () => {});
-});
-
-describe("basic route tests", () => {
-  test("get home route GET /", async () => {
-    const response = await request(app).get("/");
-    const home =
-      '{"home":{"message":"Welcome to AD SHORT-URL API","urlList":"/url"}}';
-    expect(response.status).toEqual(200);
-    expect(response.text).toContain(home);
-  });
-});
-
-/**
- * Testing root endpoint
- */
-describe("GET route /", () => {
-  it("responds with a welcoming message", done => {
-    request(app)
-      .get("/")
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200, done);
-  });
-});
-
-/**
- * Testing get all url
- */
-describe(`GET route ${ROUTE_URL}`, () => {
-  it("respond with json containing a list of all urls", done => {
-    request(app)
-      .get(ROUTE_URL)
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200, done);
+  describe("basic route tests", () => {
+    test("get home route GET /", async () => {
+      const response = await request(app).get("/");
+      const home =
+        '{"home":{"message":"Welcome to AD SHORT-URL API","urlList":"/url"}}';
+      expect(response.status).toEqual(200);
+      expect(response.text).toContain(home);
+    });
   });
 
-  // it("throws if network is down", done => {
-  //   request(app)
-  //     .get(ROUTE_URL)
-  //     .set("DENY", "application/json")
-  //     .expect("Content-Type", /json/)
-  //     .expect(500, done);
-  // });
-});
+  /**
+   * Testing root endpoint
+   */
+  describe("GET route /", () => {
+    it("responds with a welcoming message", done => {
+      request(app)
+        .get("/")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200, done);
+    });
+  });
 
-/**
- * Testing get url redirection
- */
-describe("GET /:id", function() {
-  it("respond with json containing a single url", function(done) {
-    request(app)
-      .get("/url")
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200, done);
+  /**
+   * Testing get all url
+   */
+  describe(`GET route ${ROUTE_URL}`, () => {
+    it("respond with json containing a list of all urls", done => {
+      request(app)
+        .get(ROUTE_URL)
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200, done);
+    });
+
+    // it("throws if network is down", done => {
+    //   request(app)
+    //     .get(ROUTE_URL)
+    //     .set("DENY", "application/json")
+    //     .expect("Content-Type", /json/)
+    //     .expect(500, done);
+    // });
+  });
+
+  /**
+   * Testing get url redirection
+   */
+  describe("GET /:id", function() {
+    it("respond with json containing a single url", function(done) {
+      request(app)
+        .get("/url")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200, done);
+    });
   });
 });
